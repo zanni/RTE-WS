12d375b7ff36e76077c77beae2b701616b1548cc
bzanni <zanni.bertrand@gmail.com>
2013-08-21 20:42:17 +0200

    loader.io verification controller

---
+++ src/main/java/com/rte/business/controller/website/LoaderIOController.java
@@ -0,0 +1,15 @@
+package com.rte.business.controller.website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/loaderio-eb2541ffba3454e53aef02e3241d5f36.txt")
+public class LoaderIOController {
+	@RequestMapping
+	@ResponseBody
+	public String index() {
+		return "loaderio-eb2541ffba3454e53aef02e3241d5f36";
+	}
+}
